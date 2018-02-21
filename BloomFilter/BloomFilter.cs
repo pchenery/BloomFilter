6e944d6df75f85ee9dd1aef010fa028205e51f3d
Peter Chenery <peter.chenery@anthonynolan.org>
2018-02-21 12:17:25 +0000

    Initial Version

---
+++ BloomFilter/BloomFilter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BloomFilter
+{
+    public class BloomFilter
+    {
+        private readonly BitArray bitArray;
+
+        public BloomFilter(int size)
+        {
+            bitArray = new BitArray(size);
+        }
+
+        public void Add(string input)
+        {
+            var MD5 = MD5Hash(input);
+            bitArray.Set(MD5, true);
+
+            var SHA256 = SHA256Hash(input);
+            bitArray.Set(SHA256, true);
+        }
+        private static int MD5Hash(string input)
+        {
+            var MD5Hash = MD5.Create();
+            var hash = MD5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            var hashValue = BitConverter.ToUInt16(hash, 0);
+
+            return Math.Abs(hashValue);
+        }
+        private static int SHA256Hash(string input)
+        {
+            var SHA256Hash = SHA256.Create();
+            var hash = SHA256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            var hashValue = BitConverter.ToUInt16(hash, 0);
+
+            return Math.Abs(hashValue);
+        }
+
+        public bool MightContain(string input)
+        {
+            var MD5 = MD5Hash(input);
+            var SHA256 = SHA256Hash(input);
+
+            return bitArray.Get(MD5) && bitArray.Get(SHA256);
+        }
+    }
+}
---
+++ BloomFilter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloomFilter
+{
+    class Program
+    {
+        static string[] words;
+
+        static void Main(string[] args)
+        {
+
+            words = System.IO.File.ReadAllLines(@"C:\Work\Training\Bloom\wordlist.txt");
+            BloomFilter bFilter = new BloomFilter(1000000);
+
+            foreach (var item in words)
+            {
+                bFilter.Add(item);
+            }
+
+            var test = bFilter.MightContain("stinkbug");
+            var check = listContains("stinkbug");
+            Console.WriteLine("Done");
+            Console.ReadLine();
+
+        }
+
+        public static bool listContains(string word)
+        {
+            if (words.Contains(word)) return true;
+            else return false;
+        }
+    }
+}
